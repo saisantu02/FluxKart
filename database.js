95e4b8289fe2ba2ef5d3361633f3a9036b5f3acd
Sai Santhosh <saisantu02@gmail.com>
2023-07-19 11:25:31 +0530

    add mySQL database connection

---
+++ database.js
@@ -0,0 +1,11 @@
+const sql = require('mysql2/promise');
+const connection = sql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'MIG20@home',
+    database: 'fluxkart'
+
+})
+
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const connection = require("./database");
 
-app.get("/",(req,res) => {
-    res.send("Wassup in browser");
-})
 
-app.listen(3000, () =>{
-    console.log(`Application is running on: localhost:3000`);
-})
+app.get("/", (req, res) => {
+  connection
+    .query("SELECT * from Contact")
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+
+  res.send("Wassup in browser");
+});
+
+app.listen(3000, () => {
+  console.log(`Application is running on: localhost:3000`);
+});
